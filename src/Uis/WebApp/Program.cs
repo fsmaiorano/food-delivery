bbb671062a68295dda4ff64b445be20597aeaa96
Fábio Maiorano <fsmaiorano@gmail.com>
2025-07-11 08:49:44 +0100

    feat: initialize ASP.NET web application structure with basic layout, views, and configuration files

---
+++ src/Uis/WebApp/Program.cs
@@ -0,0 +1,31 @@
+using Microsoft.AspNetCore.Hosting.StaticWebAssets;
+
+var builder = WebApplication.CreateBuilder(args);
+StaticWebAssetsLoader.UseStaticWebAssets(builder.Environment, builder.Configuration);
+
+builder.Services.AddControllersWithViews();
+
+
+var app = builder.Build();
+
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Home/Error");
+    app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+app.UseRouting();
+
+app.UseAuthorization();
+
+app.MapStaticAssets();
+
+
+app.MapControllerRoute(
+        name: "default",
+        pattern: "{controller=Home}/{action=Index}/{id?}")
+    .WithStaticAssets();
+
+
+app.Run();

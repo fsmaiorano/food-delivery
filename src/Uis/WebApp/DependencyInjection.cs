e38d95d6adec5273e287a788b3eaf7fe756af42a
Fábio Maiorano <fsmaiorano@gmail.com>
2025-07-11 18:13:23 +0100

    remove aspnet

--- src/Uis/WebApp/DependencyInjection.cs
+++
@@ -1,6 +0,0 @@
-namespace WebApp;
-
-public class DependencyInjection
-{
-    
-}
--- src/Uis/WebApp/Models/AuthenticationViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace WebApp.Models;
-
-public class AuthenticationViewModel
-{
-    public string Email { get; set; } = string.Empty;
-    public string Password { get; set; } = string.Empty;
-    public bool RememberMe { get; set; } = false;
-    public string? ReturnUrl { get; set; } = string.Empty;
-}
